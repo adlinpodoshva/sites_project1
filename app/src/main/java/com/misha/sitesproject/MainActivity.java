d06e4af026d348b7555d862570a9ecf5a653a740
adlinpodoshva <adlin183567@gmail.com>
2020-04-20 18:44:45 +0300

    fixed errors in site data

--- app/src/main/java/com/misha/sitesproject/MainActivity.java
+++ app/src/main/java/com/misha/sitesproject/MainActivity.java
@@ -1,13 +1,9 @@
 package com.misha.sitesproject;
 
 import android.content.Intent;
-
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.misha.sitesproject.area_activities.AreaSelectionActivity;
 import com.misha.sitesproject.authentication_activities.SignupActivity;
 
